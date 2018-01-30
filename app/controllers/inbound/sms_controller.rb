4250ad3ea7b0c45e35a1db3306444731f720d776
Greg Fox <gfox@gf-tech.com>
2018-01-30 13:01:16 -0800

    sms handler

--- app/controllers/inbound/sms_controller.rb
+++ app/controllers/inbound/sms_controller.rb
@@ -1,2 +1,11 @@
 class Inbound::SmsController < ApplicationController
+  def create
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+    # messages_endpoint.create(to: to_number, from: from_number, body: body, status_callback: status_callback)
+    @client.api.account.messages.create(
+      to: '+',
+      from: '+',
+      body: 'Hey there!'
+    )
+  end
 end
